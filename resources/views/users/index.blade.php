a65877313c747316ce4824233fdfe7aa2b711405
Gerardo Belot <gerardo.belot@gmail.com>
2021-02-25 02:19:39 +0000

    :sparkles: ajustando livewire datatable he iseeder

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,11 +4,11 @@
             {{ __('Users Administration') }}
         </h2>
     </x-slot>
-
+    
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                
+                <livewire:user-data-table hide-header hide-pagination />
             </div>
         </div>
     </div>
